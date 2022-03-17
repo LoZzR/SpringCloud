032510b67af1758baa8223de82f825775f82c82d
LoZzR <zakariaelhichem@gmail.com>
2022-03-17 19:53:51 +0100

    Create discovery Eureka server

---
+++ discovery-server/src/main/java/com/discovery/server/DiscoveryServerApplication.java
@@ -0,0 +1,24 @@
+package com.discovery.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import java.io.IOException;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+	private static Logger logger = LoggerFactory.getLogger(DiscoveryServerApplication.class);
+	public static void main(String... args) throws IOException {
+		// Look for configuration in discovery.properties or discoveryserver.yml
+		System.setProperty("spring.config.name", "discovery-server.yml");
+		var ctx = SpringApplication.run(DiscoveryServerApplication.class, args);
+		assert (ctx != null);
+		logger.info("Started ...");
+		System.in.read();
+		ctx.close();
+	}
+}
